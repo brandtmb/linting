305fa09e2b68651d2f55ff7cc733f9e3731bcace
brandtmb <114689646+brandtmb@users.noreply.github.com>
2023-03-13 00:46:32 +0000

    added users section

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.set('view engine','ejs');
 //this will allow us to serve up static files, CSS, images & JS 
 app.use(express.static(__dirname)); 
 
+//Here's a JSON file of user data
+var data = require('./data/test.json'); 
 
 app.get('/', (req, res) => { 
   var title = "Home Page";
@@ -16,6 +18,15 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => { 
   var title = "About Page";
   res.render('pages/about',{'title':title});
+}); 
+
+//users index is our list page 
+app.get('/users', function(req, res) { 
+	var title = 'Users Page'; 
+	res.render('users/index', { 
+    	title: title, 
+    	users: data 
+	}); 
 }); 
 
 app.get('/contact', (req, res) => { 
@@ -28,7 +39,19 @@ app.get('/projects', (req, res) => {
   res.render('pages/projects',{'title':title});
 }); 
 
+ 
+//add user/view route - we are cheating by using the array index - 1 
+app.get('/users/view/:id', function(req, res) { 
+ var title = 'User Page'; 
+ var id = req.params.id; 
+ res.render('users/view', { 
+     title: title, 
+     user: data[--id] 
+ }); 
+}); 
+
 app.listen(port, () => { 
   console.log(`Example app listening on port ${port}`); 
+    console.log(data);
 }); 
 
